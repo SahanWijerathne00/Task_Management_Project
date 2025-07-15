57ad5bc0aee7692071a5513f1d317fefb54b5957
Sahan Wijerathne <202465877+SahanWijerathne00@users.noreply.github.com>
2025-07-15 21:58:24 +0530

    Update the files

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskmanagementproject.up.railway.app/api",
 });
 
-// Add token to all requests if available
+// Automatically attach JWT token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
